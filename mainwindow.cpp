74875e0054bb2bbbf13dd953b2df9f488ed33c94
Loayelshall <Loay.elshall@icloud.com>
2021-04-23 02:31:44 +0200

    comments added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QVector2D>
 
+
 QVector<QVector<QString>> processes;
 QString test = "test";
 
@@ -23,6 +24,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+    // Taking input from user and saving it in a vector
     QString processName = ui->proccesName->text();
     QString arrivalTime = ui->arrivalTime->text();
     QString burstTime = ui->burstTime->text();
@@ -39,3 +41,23 @@ void MainWindow::on_pushButton_clicked()
 
 
 }
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    // Hide process inputs
+    ui->proccesName->hide();
+    ui->arrivalTime->hide();
+    ui->burstTime->hide();
+    ui->label->hide();
+    ui->label_2->hide();
+    ui->label_3->hide();
+    ui->label_4->hide();
+    ui->textBrowser->hide();
+    ui->pushButton->hide();
+    ui->pushButton_2->hide();
+
+    // Display Chart
+
+
+}
+
