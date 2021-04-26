e6554ddf97b15f5cc8bdc12fd054ed99246658d3
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2021-04-26 06:34:59 +0200

    FCFS Co-authored-by: amr1412 <amrabdulaziz98@gmail.com>

--- backend/FCFS.cpp
+++ backend/FCFS.cpp
@@ -17,6 +17,26 @@ int FCFS::get_input_burst_and_arrival_time(vector<process> &fcfs_process, int nu
     return sum_of_burst_time;
 }
 
+void FCFS::calc_new_order(vector<process> &fcfs_process, vector<process> &fcfs_process_new, int num_of_proceses)
+{
+    int sum_of_burst_time = 0;
+    sort(fcfs_process.begin(), fcfs_process.end(), [](process a, process b) { return a.arrival_time < b.arrival_time; });
+    for (size_t i = 0; i < num_of_proceses; i++)
+    {
+        process temp_process = fcfs_process[i];
+        if (i == 0 || temp_process.arrival_time > fcfs_process[i - 1].start_time + fcfs_process[i - 1].burst_time)
+        {
+            temp_process.start_time = temp_process.arrival_time;
+            fcfs_process_new.push_back(temp_process);
+        }
+        else
+        {
+            temp_process.start_time = fcfs_process[i - 1].start_time + fcfs_process[i - 1].burst_time;
+            fcfs_process_new.push_back(temp_process);
+        }
+    }
+}
+
 void FCFS::calc_waiting_time(vector<process> &fcfs_process, int num_of_proceses)
 {
     int sum_of_burst_time = 0;
